e8ff034f694039970a929563ce29c6eda4fc793d
I-like-pelmeni <maxtoo2017@gmail.com>
2022-01-15 18:45:50 +0200

    Add ship task

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
 
@@ -23,26 +24,32 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
-
-            # Отображение последнего прорисованного экрана
-            pygame.display.flip()
-
     def _check_events(self):
-        """Обрабвтывает нажатия клавиш и события мыши."""
+        """Обрабатывает нажатия клавиш и события мыши."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Переместить корабль вправо
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
-        """Обрабатывает изображения на экране и отображает новый экран."""
+        """Обновляет изображения на экране и отображает новый экран."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
