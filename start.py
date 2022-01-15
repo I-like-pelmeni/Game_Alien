ddc50a76a1ab84caee7adf83317ca9b9d4513725
I-like-pelmeni <maxtoo2017@gmail.com>
2022-01-15 17:46:44 +0200

    Add ship task

--- start.py
+++ start.py
@@ -2,26 +2,36 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
-    """Класс для управления ресурсами и поведлениеи игры."""
+    """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
-        """Иницивлизирует игру и создает игровые ресурсы"""
+        """Инициалищирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
-        """Запуск основного цикла игры"""
+        """Запуск основного цикла игры."""
         while True:
             # Отслеживание событий клавиатуры и мыши
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Отображение последнего прорисованого экрана
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # Отображение последнего прорисованного экрана
             pygame.display.flip()
 
 
