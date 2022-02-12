823cbcc28ada5d2c3cfd185bcf27a8c0e15c1324
I-like-pelmeni <maxtoo2017@gmail.com>
2022-02-12 17:02:38 +0200

    Add start task

--- start.py
+++ start.py
@@ -56,7 +56,30 @@ class AlienInvasion:
                 self._check_keydown_events(event)    
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-    
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Запускает новую игру при нажатии кнопки Play"""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Сброс игровой статистики
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            # Очитска пришельцев и снарядов
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Создание нового флота и размещение корабля в центре
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Указатель миши скрывается
+            pygame.mouse.set_visible(False)
+
+
     def _check_keydown_events(self, event):
         """Реагирует на нажати клавиш."""
         if event.key == pygame.K_RIGHT:
@@ -136,6 +159,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         """Создание флота вторжения."""
