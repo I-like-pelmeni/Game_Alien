e8d20abb567c427cf450e2c430357eb57cbf848d
I-like-pelmeni <maxtoo2017@gmail.com>
2022-02-12 18:53:09 +0200

    Add task

--- start.py
+++ start.py
@@ -72,6 +72,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Очитска пришельцев и снарядов
             self.aliens.empty()
@@ -137,6 +138,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+            
+            # Увеличение уровня
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев во флоте"""
