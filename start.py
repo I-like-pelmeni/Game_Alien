0806a8e9100aed4255adc3b52da536e58c981d41
I-like-pelmeni <maxtoo2017@gmail.com>
2022-02-12 17:16:54 +0200

    Add start task

--- start.py
+++ start.py
@@ -64,7 +64,8 @@ class AlienInvasion:
         """Запускает новую игру при нажатии кнопки Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-            # Сброс игровой статистики
+            # Сброс игровой статистики и настроек
+            self.settings.initialyze_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -76,7 +77,7 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
-            # Указатель миши скрывается
+            # Указатель мыши скрывается
             pygame.mouse.set_visible(False)
 
 
@@ -125,6 +126,7 @@ class AlienInvasion:
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев во флоте"""
